b61f408229050dc5174a66497b769a9133c55eda
Martin Logan <martinjlogan@Macintosh.local>
2009-06-21 13:54:33 -0500

    synced with chapter 9

--- simple_cache/lib/simple_cache/src/sc_element.erl
+++ simple_cache/lib/simple_cache/src/sc_element.erl
@@ -97,7 +97,6 @@ delete(Pid) ->
 %% @end
 %%--------------------------------------------------------------------
 init([Value]) ->
-    sc_store:insert(self()), %% This should be the last thing we do in init/1.
     {ok, #state{value = Value}}.
 
 %%--------------------------------------------------------------------
