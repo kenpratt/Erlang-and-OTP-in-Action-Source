9d9271807c434d8482f859a67ee6144ee74d0063
Richard Carlsson <carlsson.richard@gmail.com>
2010-03-16 20:30:33 +0100

    updated ts_server to match code in book and chapter 3

--- telnet_server/lib/telnet_server/src/tr_server.erl
+++ telnet_server/lib/telnet_server/src/tr_server.erl
@@ -2,28 +2,29 @@
 %%% @author Martin & Eric <erlware-dev@googlegroups.com>
 %%%  [http://www.erlware.org]
 %%% @copyright 2008-2010 Erlware
-%%% @doc This module defines a server process that listens for incoming
-%%%      TCP connections and allows the user to execute commands via
-%%%      that TCP stream 
+%%% @doc RPC over TCP server. This module defines a server process that
+%%%      listens for incoming TCP connections and allows the user to
+%%%      execute RPC commands via that TCP stream.
 %%% @end
 %%%-------------------------------------------------------------------
--module(ts_server).
+
+-module(tr_server).
 
 -behaviour(gen_server).
 
 %% API
 -export([
-	 start_link/0,
-	 start_link/1,
-	 get_count/0,
-	 stop/0
-	]).
+         start_link/0,
+         start_link/1,
+         get_count/0,
+         stop/0
+        ]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-	 terminate/2, code_change/3]).
+         terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE). 
+-define(SERVER, ?MODULE).
 -define(DEFAULT_PORT, 1055).
 
 -record(state, {port, lsock, request_count = 0}).
@@ -33,10 +34,11 @@
 %%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @doc
-%% Starts the server
+%% @doc Starts the server.
 %%
-%% @spec start_link(Port::integer()) -> {ok, Pid} | ignore | {error, Error}
+%% @spec start_link(Port::integer()) -> {ok, Pid}
+%% where
+%%  Pid = pid()
 %% @end
 %%--------------------------------------------------------------------
 start_link(Port) ->
@@ -48,7 +50,7 @@ start_link() ->
     start_link(?DEFAULT_PORT).
 
 %%--------------------------------------------------------------------
-%% @doc fetch the number of requests made to this server.
+%% @doc Fetches the number of requests made to this server.
 %% @spec get_count() -> {ok, Count}
 %% where
 %%  Count = integer()
@@ -58,7 +60,7 @@ get_count() ->
     gen_server:call(?SERVER, get_count).
 
 %%--------------------------------------------------------------------
-%% @doc stops the server
+%% @doc Stops the server.
 %% @spec stop() -> ok
 %% @end
 %%--------------------------------------------------------------------
