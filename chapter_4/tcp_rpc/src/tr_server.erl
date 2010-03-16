5c563133c19a092f102928ea08ad23ab31dc7c19
Richard Carlsson <carlsson.richard@gmail.com>
2010-03-16 21:01:09 +0100

    fixed an edoc-error

--- chapter_4/tcp_rpc/src/tr_server.erl
+++ chapter_4/tcp_rpc/src/tr_server.erl
@@ -49,7 +49,7 @@ start_link(Port) ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [Port], []).
 
 %% @spec start_link() -> {ok, Pid}
-%% @equiv start_link(Port::integer())
+%% @doc Calls `start_link()' using the default port.
 start_link() ->
     start_link(?DEFAULT_PORT).
 
