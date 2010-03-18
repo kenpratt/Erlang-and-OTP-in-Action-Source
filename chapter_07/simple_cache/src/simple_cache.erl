98f9445f665338d52c2094b696208d9dde4a3b64
Richard Carlsson <carlsson.richard@gmail.com>
2010-03-18 20:26:06 +0100

    fixed error in code

--- chapter_07/simple_cache/src/simple_cache.erl
+++ chapter_07/simple_cache/src/simple_cache.erl
@@ -5,14 +5,16 @@
 insert(Key, Value) ->
     case sc_store:lookup(Key) of
         {ok, Pid} ->
+            sc_event:replace(Key, Value),
             sc_element:replace(Pid, Value);
         {error, _} ->
             {ok, Pid} = sc_element:create(Value),
             sc_store:insert(Key, Pid),
-            sc_event:create(Key)
+            sc_event:create(Key, Value)
     end.
 
 lookup(Key) ->
+    sc_event:lookup(Key),
     try
         {ok, Pid} = sc_store:lookup(Key),
         {ok, Value} = sc_element:fetch(Pid),
@@ -23,6 +25,7 @@ lookup(Key) ->
     end.
 
 delete(Key) ->
+    sc_event:delete(Key),
     case sc_store:lookup(Key) of
         {ok, Pid} ->
             sc_element:delete(Pid);
