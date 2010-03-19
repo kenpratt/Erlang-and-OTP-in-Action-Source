8210c687cda32a4e32ebdc7e71979c592af1f3ad
Richard Carlsson <carlsson.richard@gmail.com>
2010-03-19 22:27:04 +0100

    minor correction

--- chapter_09/resource_discovery/src/rd_app.erl
+++ chapter_09/resource_discovery/src/rd_app.erl
@@ -5,7 +5,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    case tr_sup:start_link() of
+    case rd_sup:start_link() of
         {ok, Pid} ->
             {ok, Pid};
         Other ->
