65d46dec09c43960a86f4979726fdb0ee7b56eb0
Richard Carlsson <carlsson.richard@gmail.com>
2010-04-24 19:42:48 +0200

    replaced old simple_cache.erl file with modified version for chapter 13

--- chapter_13/simple_cache/src/simple_cache.erl
+++ chapter_13/simple_cache/src/simple_cache.erl
@@ -2,11 +2,14 @@
 
 -export([insert/2, lookup/1, delete/1]).
 
+-define(HBASE_NODE, hbase).
+
 insert(Key, Value) ->
     case sc_store:lookup(Key) of
         {ok, Pid} ->
             sc_event:replace(Key, Value),
-            sc_element:replace(Pid, Value);
+            sc_element:replace(Pid, Value),
+	    sc_hbase:put(?HBASE_NODE, Key, Value);
         {error, _} ->
             {ok, Pid} = sc_element:create(Value),
             sc_store:insert(Key, Pid),
@@ -16,9 +19,15 @@ insert(Key, Value) ->
 lookup(Key) ->
     sc_event:lookup(Key),
     try
-        {ok, Pid} = sc_store:lookup(Key),
-        {ok, Value} = sc_element:fetch(Pid),
-        {ok, Value}
+        case sc_store:lookup(Key) of
+            {ok, Pid} ->
+                {ok, Value} = sc_element:fetch(Pid),
+                {ok, Value};
+            {error, _} ->
+                {ok, Value} = sc_hbase:get(?HBASE_NODE, Key),
+                insert(Key, Value),
+                {ok, Value}
+        end
     catch
         _Class:_Exception ->
             {error, not_found}
@@ -28,6 +37,7 @@ delete(Key) ->
     sc_event:delete(Key),
     case sc_store:lookup(Key) of
         {ok, Pid} ->
+	    sc_hbase:delete(?HBASE_NODE, Key),
             sc_element:delete(Pid);
         {error, _Reason} ->
             ok
