1a8c92d229af5dce4122cfe5733fde002de518a1
Richard Carlsson <carlsson.richard@gmail.com>
2010-05-13 16:48:24 +0200

    updated active_once.erl example to match code in book

--- chapter_11/active_once.erl
+++ chapter_11/active_once.erl
@@ -1,22 +1,18 @@
 -module(active_once).
+
 -export([start/0]).
 
 start() ->
     {ok, LSock} = gen_tcp:listen(1055, [binary, {active, false}]),
-    accept(LSock).
-
-accept(LSock) ->
     {ok, Socket} = gen_tcp:accept(LSock),
-    inet:setopts(Socket,[{active,once}]),
-    loop().
+    loop(Socket).
 
-loop() ->
+loop(Socket) ->
+    inet:setopts(Socket, [{active,once}]),
     receive
-	{tcp, Socket, Packet} ->
-	    io:format("got ~p~n", [Packet]),
-	    inet:setopts(Socket,[{active,once}]),
-	    loop();
-	{tcp_closed, _Socket} ->
-	    ok
+        {tcp, Socket, Data} ->
+            io:format("got ~p~n", [Data]),
+            loop(Socket);
+        {tcp_closed, _Socket} ->
+            ok
     end.
-
