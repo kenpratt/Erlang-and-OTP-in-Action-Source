07aa67394a7f4ccc3dea65288f25c392436662d5
Ken Pratt <ken@kenpratt.net>
2010-05-20 15:46:51 -0700

    Removed unneccessary SERVER macro definition.

--- chapter_11/tcp_interface/src/ti_server.erl
+++ chapter_11/tcp_interface/src/ti_server.erl
@@ -7,8 +7,6 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE).
-
 -record(state, {lsock}).
 
 start_link(LSock) ->
