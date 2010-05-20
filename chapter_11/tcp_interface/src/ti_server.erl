c1a24bfc4cc1b59639062867356e9edf2cee58e1
Ken Pratt <ken@kenpratt.net>
2010-05-20 16:07:30 -0700

    Modified ti_server to stop when the socket is closed.

--- chapter_11/tcp_interface/src/ti_server.erl
+++ chapter_11/tcp_interface/src/ti_server.erl
@@ -24,6 +24,8 @@ handle_cast(stop, State) ->
 handle_info({tcp, Socket, RawData}, State) ->
     NewState = handle_data(Socket, RawData, State),
     {noreply, NewState};
+handle_info({tcp_closed, _Socket}, State) ->
+    {stop, normal, State};
 handle_info(timeout, #state{lsock = LSock} = State) ->
     {ok, _Sock} = gen_tcp:accept(LSock),
     ti_sup:start_child(),
